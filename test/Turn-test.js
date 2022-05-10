294ca22e1e2a4a920ab19b13561f562e10478e4c
“Corinne <corinne.canavan@gmail.com>
2022-05-09 21:52:23 -0600

    Begin writing Turn tests

---
+++ test/Turn-test.js
@@ -0,0 +1,26 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Turn = require('../src/Turn');
+const Card = require('../src/Card');
+
+describe('Turn', () => {
+
+  it('should be a function', () => {
+    const turn = new Turn();
+    expect(Turn).to.be.a('function');
+  });
+
+  it('should be an instance of Turn', () => {
+    const turn = new Turn();
+    expect(turn).to.be.an.instanceOf(Turn);
+  });
+
+  it('should be instantiated with a string and a card object', () => {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object')
+    const turn = new Turn('object', card);
+
+    expect(turn.guess).to.be.a('string');
+    expect(turn.card).to.be.an('object');
+  })
+});
