be5f7c9f575754f8a4f06e84aee3db427d0272b1
“Corinne <corinne.canavan@gmail.com>
2022-05-12 01:41:52 -0600

    Add tests to confirm currentRound property; add implementation code; invoke start method in index.js

--- test/Game-test.js
+++ test/Game-test.js
@@ -2,17 +2,8 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Game = require('../src/Game');
-const Round = require('../src/Round');
-const Card = require('../src/Card');
-const Deck = require('../src/Deck');
-const Turn = require('../src/Turn');
 
 describe('Game', () => {
-  let card1;
-  let card2;
-  let card3;
-  let deck;
-  let round;
   let game;
 
   it('should be a function', () => {
@@ -24,13 +15,11 @@ describe('Game', () => {
     expect(game).to.be.an.instanceOf(Game);
   });
 
-  beforeEach(() => {
-    card1 = new Card(1, 'What animal cannot breathe through its mouth?', ['dog', 'horse', 'elephant'], 'horse');
-    card2 = new Card(2, 'What is the only mammal that can fly?', ['flying squirrel', 'bat', 'monkey'], 'bat');
-    card3 = new Card(3, 'What is a group of owls called?', ['murder', 'gaggle', 'parliament'], 'parliament');
-    deck = new Deck([card1, card2, card3]);
-    round = new Round(deck.cardSet);
-    game = new Game();
+  it('should have a currentRound property', () => {
+    expect(game).to.have.property('currentRound');
   });
 
+  it('should initialize currentRound as null', () => {
+    expect(game.currentRound).to.equal(null);
+  });
 });
